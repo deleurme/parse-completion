6e363232bce9b212885750f7b4d653821af7c2fd
Shawn Deleurme <shawn@deleur.me>
2016-02-25 19:26:55 -0800

    Fixed issue with linux

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 OS=`uname`
 
-if [ $OS == "Darwin" -a -x `which brew` ]; then
+if [ $OS == "Darwin" ] && [  -x $(which brew)  ]; then
   cp parse_completion /usr/local/etc/bash_completion.d
 else 
   cp parse_completion /etc/bash_completion.d/
